9942663171f762735699f4eae4734f2f2900b6b7
Yakov-Varnaev <yakov.varnaev@gmail.com>
2024-11-13 16:03:16 +0300

    feat: make signin form flat

--- components/signin/card.vue
+++ components/signin/card.vue
@@ -1,9 +1,7 @@
 <template>
-  <div>
-    <v-card class="pa-2">
-      <v-card-title> SignIn</v-card-title>
-      <signin-form />
-    </v-card>
-    <v-btn class="mt-2" block variant="text" to="signup">signup</v-btn>
-  </div>
+  <v-card class="pa-2" variant="flat">
+    <v-card-title> SignIn</v-card-title>
+    <signin-form />
+  </v-card>
+  <v-btn class="mt-2" block variant="text" to="signup">signup</v-btn>
 </template>
